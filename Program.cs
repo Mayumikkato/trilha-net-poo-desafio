252e84e6fc0680baffb4c69a8d96e0fd0b170b0e
Fernanda Mayumi <128170082+Mayumikkato@users.noreply.github.com>
2024-01-14 00:36:20 -0300

    Add files via upload
    desafio concluído!!!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(novoNumero: "1199999-9999", novoModelo: "Nokia G11 Plus", novoImei: "123456789123456", NovaMemoria: 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(novoNumero: "118888-8888", novoModelo: "Iphone 15 Plus", novoImei: "987654321987654", NovaMemoria: 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Clash Royale");
